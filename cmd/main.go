d802e66c1445898d8c0fada1bfd13fb795c1e034
Jane <jane@MacBook-Pro2.local>
2022-11-24 18:26:14 +0300

    add keyboard in /go

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,7 @@ import (
 	"bot/parsing"
 	"fmt"
 	"log"
-
+	"time"
 	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -17,22 +17,26 @@ type Form struct {
 }
 
 const adminChatID int64 = -1001602774786
+const expeditionUrl = "https://russiantravelgeek.com/expeditions/"
 
 func main() {
-
+	fmt.Println("Started")
 	bot, err := tgbotapi.NewBotAPI("5741027893:AAHgH5pyL7gQWm8MLyTuuG7lO9ftUvAliyQ") //защитить
 	if err != nil {
 		log.Panic(err)
 	}
-	bot.Debug = true
+	//bot.Debug = true
 	//log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
+	expeditionList := parsing.ParseHTML(expeditionUrl)
+	go continiousParseExpedition (expeditionList)
+	fmt.Println(expeditionList)
+
 	var form Form
-	//type form map[string]string
-	//var bd map[int64]form
+
 	updates := bot.GetUpdatesChan(u)
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message updates
@@ -52,12 +56,11 @@ func main() {
 			find(msg, bot)
 		case "go":
 			form.tg_name = update.Message.From.UserName
-			ask(msg, bot, updates, &form, "Куда идем?")
+			ask_expediton(msg, bot, updates, &form, expeditionList)
 			ask(msg, bot, updates, &form, "Имя и фамилия:")
 			ask(msg, bot, updates, &form, "Электронный адрес:")
 			ask(msg, bot, updates, &form, "Возраст:")
-			//add_in_bd((bot,form))
-			check_info(msg, bot, form) //todo: add ability to make changes in form/refill form
+			check_info(msg, bot, form)
 			//send_info(form)
 			//https://t.me/+aCYdv4e0hmw0NWUy
 		default:
@@ -66,10 +69,15 @@ func main() {
 	}
 }
 
-//	func send_info(form Form) {
-//		//https://api.telegram.org/bot5741027893:AAHgH5pyL7gQWm8MLyTuuG7lO9ftUvAliyQ/sendMessage?chat_id=[MY_CHANNEL_NAME]&text="hi"
-//		//id -1001602774786
-//	}
+func continiousParseExpedition (expeditionList []parsing.Expedition) {
+	c := time.Tick(1 * time.Minute)
+	for now := range c {
+			fmt.Println("tick", now)
+			expeditionList = parsing.ParseHTML(expeditionUrl)
+	}
+
+}
+
 func check_info(msg tgbotapi.MessageConfig, bot *tgbotapi.BotAPI, form Form) {
 	msg.Text = fmt.Sprintf("Ник в телеграмме : %s\nЭкспедиция : %s\nИмя : %s\nEmail : %s\nВозраст : %s", form.tg_name, form.expediton, form.name, form.email, form.age)
 
@@ -84,21 +92,35 @@ func check_info(msg tgbotapi.MessageConfig, bot *tgbotapi.BotAPI, form Form) {
 }
 
 func find(msg tgbotapi.MessageConfig, bot *tgbotapi.BotAPI) {
-	expList := parsing.ParseHTML("https://russiantravelgeek.com/expeditions/")
+	expList := parsing.ParseHTML("https://russiantravelgeek.com/expeditions/")//убрать
 	msg.ParseMode = "HTML"
-	//msg.DisableWebPagePreview = false
 
-	for _, val := range expList{
-		msg.Text = "<b>Название экспеды</b> \n"
-		msg.Text += val + "\n"
-		msg.Text += "https://russiantravelgeek.com/expeditions/newyear23" + "\n"
+	//msg.DisableWebPagePreview = false
+	for _, val := range expList {
+		msg.Text = "<b>" + val.Name + "</b> \n"
+		msg.Text += val.Place + "\n"
+		msg.Text += val.Link + "\n"
 		msg.Text += "\n"
-			if _, err := bot.Send(msg); err != nil {
+
+		if _, err := bot.Send(msg); err != nil {
 			log.Panic(err)
 		}
 	}
+}
+
+func ask_expediton(msg tgbotapi.MessageConfig, bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel, form *Form, expeditionList []parsing.Expedition) {
+	msg.Text = "Куда идем?"
+	msg.ReplyMarkup = setupKeyboard(expeditionList)
+
 	if _, err := bot.Send(msg); err != nil {
 		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message != nil {
+			form.expediton = update.Message.Text
+			break
+		}
 	}
 }
 
@@ -112,8 +134,6 @@ func ask(msg tgbotapi.MessageConfig, bot *tgbotapi.BotAPI, updates tgbotapi.Upda
 		if update.Message != nil {
 			//это можно сделать без повторения списков вопросов? можно. Как?
 			switch q {
-			case "Куда идем?":
-				form.expediton = update.Message.Text
 			case "Имя и фамилия:":
 				form.name = update.Message.Text
 			case "Электронный адрес:":
@@ -121,11 +141,16 @@ func ask(msg tgbotapi.MessageConfig, bot *tgbotapi.BotAPI, updates tgbotapi.Upda
 			case "Возраст:":
 				form.age = update.Message.Text
 			}
-
 			break
 		}
 	}
-
 }
 
-//todo: add go rutines for multiple use
+func setupKeyboard(expeditionList []parsing.Expedition) tgbotapi.InlineKeyboardMarkup{
+	buttons := make([]tgbotapi.InlineKeyboardButton, 0, 10)
+	for _, expedition := range expeditionList {
+		buttons = append(buttons, tgbotapi.NewInlineKeyboardButton(expedition.Name))
+	}
+	var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(buttons)
+	return numericKeyboard
+}
